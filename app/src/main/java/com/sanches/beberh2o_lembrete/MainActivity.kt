a2294f54452fb15b1895d132f9b117c920857c68
Sanches536 <edsanches@hotmail.com>
2022-06-17 16:57:58 -0300

    Initial commit

---
+++ app/src/main/java/com/sanches/beberh2o_lembrete/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sanches.beberh2o_lembrete
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
