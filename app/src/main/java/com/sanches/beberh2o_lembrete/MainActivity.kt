7e6c84c06556b818c4aa36a9584a1439c94821b0
Sanches536 <edsanches@hotmail.com>
2022-06-18 16:35:45 -0300

    Adicionado função de alarme

--- app/src/main/java/com/sanches/beberh2o_lembrete/MainActivity.kt
+++ app/src/main/java/com/sanches/beberh2o_lembrete/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.sanches.beberh2o_lembrete
 
 import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.sanches.beberh2o_lembrete.databinding.ActivityMainBinding
@@ -14,6 +18,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
+
+    lateinit var timePickerDialog: TimePickerDialog
+    lateinit var calendar: Calendar
+    var horaAtual = 0
+    var minutoAtual = 0
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +65,33 @@ class MainActivity : AppCompatActivity() {
             val dialog = alertDialog.create()
             dialog.show()
         }
+
+        binding.btnLembrete.setOnClickListener{
+            calendar = Calendar.getInstance()
+            horaAtual = calendar.get(Calendar.HOUR_OF_DAY)
+            minutoAtual = calendar.get(Calendar.MINUTE)
+            timePickerDialog = TimePickerDialog(this,{ timePicker: TimePicker, hourOfDay: Int, minutes: Int ->
+                binding.txtHora.text = String.format("%02d", hourOfDay)
+                binding.txtMinuto.text = String.format("%02d", minutes)
+            },horaAtual, minutoAtual, true)
+            timePickerDialog.show()
+        }
+
+        binding.btnAlarme.setOnClickListener {
+
+                if (binding.txtHora.toString().isNotEmpty() && binding.txtMinuto.toString().isNotEmpty()){
+                val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+                intent.putExtra(AlarmClock.EXTRA_HOUR, binding.txtHora.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MINUTES, binding.txtMinuto.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MESSAGE, getString(R.string.alarme_mensagem))
+                startActivity(intent)
+
+                if (intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+            }
+        }
+
 
     }
 }
